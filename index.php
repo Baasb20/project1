6d4b04ebe561e035ed7bd33c9f18b5e68359991b
Baasb20 <2058682@talnet.nl>
2020-09-15 15:50:30 +0200

    ik heb gewerkt aan de forms opdracht, Week 1 les 2 -Forms

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <body>
+    <h1>Login</h1>
+    <form>
+      <label>Gebruikersnaam: </label>
+        <input type="text" name="gebruikersnaam" placeholder="Uw gebruikersnaam" required><br>
+      <label>Wachtwoord: </label>
+        <input type="password" name="wachtwoord" placeholder="Uw wachtwoord"required><br>
+      <button type="submit">Inloggen</button><br>
+      Geen account? <a href="signup.php">Sign Up</a><br>
+      Wachtwoord vergeten? <a href="lostpsw.php">Reset wachtwoord</a>
+    </form>
+  </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,22 @@
+<html>
+  <body>
+    <h1>Sign Up</h1>
+    <form>
+        <label>Voornaam:</label>
+          <input type="text" name="voornaam" placeholder="Vul in uw gebruikersnaam" required><br>
+        <label>Tussenvoegsel:</label>
+          <input type="text" name="Tussenvoegsel" placeholder="Vul in uw tussenvoegsel"><br>
+        <label>Achtrnaam:</label>
+          <input type="text" name="achternaam" placeholder="Vul in uw achternaam" required><br>
+        <label>E-mail:</label>
+          <input type="email" name="e-mail" placeholder="Vul in uw E-mail" required><br>
+        <label>Gebruikersnaam:</label>
+          <input type="text" name="gebruikersnaam" placeholder="Vul in uw gebruikersnaam" required><br>
+        <label>Wachtwoord:</label>
+          <input type="password" name="wachtwoord" required><br>
+        <label>Herhaal wachtwoord:</label>
+          <input type="password" name="herhaal-wachtwoord" required><br>
+        <button type="submit" href="index.php">Sign in</button>
+    </form>
+  </body>
+</html>
